841574b8f9ac35a588ecfb239431e3a0633f7e33
Onyanta <meetbigdudes@gmail.com>
2026-01-17 23:13:52 +0000

    updates

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region  = "us-east-1"
-  # profile = "sock_shop"
+   profile = "sock_shop"
 }
 
 terraform {
   backend "s3" {
-    bucket       = "sock-shop-team33"
+    bucket       = "sock-shop-bkt-aleyi"
     key          = "infrastructure/terraform.tfstate"
     region       = "us-east-1"
-    # profile      = "sock_shop"
+     profile      = "sock_shop"
     encrypt      = true
     use_lockfile = true
   }
