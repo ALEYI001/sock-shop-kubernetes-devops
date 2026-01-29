56b60401fefbf7271c6da83e440b2c4db17c7478
Onyanta <meetbigdudes@gmail.com>
2026-01-29 13:26:07 +0000

    bastion role and ansible role update

--- module/bastion/main.tf
+++ module/bastion/main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "bastion_sg" {
 
 # IAM Role 
 resource "aws_iam_role" "bastion_role" {
-  name = "${var.name}-bastion-role"
+  name = "${var.name}-bastion-role1"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
